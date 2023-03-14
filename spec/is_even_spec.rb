ce5df3c35ed4fed386230d076df816bf7be7fb20
marie taylor <marietaylortailor@hotmail.com>
2023-03-14 18:28:32 +0000

    passing first test

--- spec/is_even_spec.rb
+++ spec/is_even_spec.rb
@@ -1,3 +1,9 @@
 require 'is_even'
 
-Rspec
+RSpec.describe 'is_even' do
+    describe 'is_even' do
+      it 'returns null when (n) is empty' do
+        expect(is_even()).to eq nil
+      end
+    end
+  end
